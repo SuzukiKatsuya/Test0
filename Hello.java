bca0ba78865bcaf43fb7ce1cce31149b0d7b511f
k16057kk <k16057@pck16057kk.local>
2017-09-13 17:14:05 +0900

    欠陥部分をコメントアウト

--- Hello.java
+++ Hello.java
@@ -3,13 +3,13 @@ class Hello{
     System.out.println("Hello World");
 
     int[] hako = new int [5];
-    //失敗作
+    /*/失敗作
     for(int i=0;i<6;i++){
       hako[i] = i+1;
     }
     for(int i=0;i<5;i++){
       Sysytem.out.println(hako[i]);
     }
-
+*/
   }
 }
