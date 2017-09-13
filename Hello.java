9f22ed8bf45e8fa239ff06f19add9f5dcbfe20cc
k16057kk <k16057@pck16057kk.local>
2017-09-13 15:57:49 +0900

    失敗作

--- Hello.java
+++ Hello.java
@@ -1,7 +1,14 @@
 class Hello{
   public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println("2");
-    //コメント
+    //失敗作
+    int[] hako = new int [5];
+
+    for(int i=0;i<6;i++){
+      hako[i] = i+1;
+    }
+    for(int i=0;i<5;i++){
+      Sysytem.out.println(hako[i]);
+    }
   }
 }
