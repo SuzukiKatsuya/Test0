68646390d8a22183262a631aae0ece5322aff37c
k16057kk <k16057@pck16057kk.local>
2017-09-13 15:59:08 +0900

    失敗作

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 class Hello{
   public static void main(String[] args) {
     System.out.println("Hello World");
-    //失敗作
-    int[] hako = new int [5];
 
+    int[] hako = new int [5];
+    //失敗作
     for(int i=0;i<6;i++){
       hako[i] = i+1;
     }
