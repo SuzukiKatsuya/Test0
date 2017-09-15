8d550df63b6407d9fe7af7530a052ea3e217785b
k16057kk <k16057@pck16057kk.local>
2017-09-15 14:09:28 +0900

    コンパイルエラーさせる部分をコメントアウト

--- Hello.java
+++ Hello.java
@@ -5,7 +5,8 @@ class Hello{
     int[] hako = new int [5];
     //失敗作
     for(int i=0;i<5;i++){
-      box[i] = i+1;
+      //box[i] = i+1;
+      hako[i] = i*11;
     }
     for(int i=0;i<5;i++){
       System.out.println(hako[i]);
--- Hello_c.c
+++ Hello_c.c
@@ -4,7 +4,8 @@ int main(void) {
   printf("HelloWorld\n");
   int a;
 
-  a=1.0;
-  printf("%d",b);   //エラー
+  a=16;
+  printf("%d",a);
+  //printf("%d",b);   //エラー
   return 0;
 }
